36fcc4cf5f50b0eb7e706a0ea27c420481d7985b
Parker <parkermatheson@gmail.com>
2018-07-25 19:27:26 -0600

    initial pseudo-code process

---
+++ most_common_element.rb
@@ -0,0 +1,39 @@
+# find the most common element in the given string
+
+# "avcc14444"
+
+str = "avcc14444"
+
+# turn string into array
+str.to_a
+
+# go through each element
+str.each do |e|
+
+# keep score
+
+# empty hash
+
+# if element is unique
+
+#   make new array
+
+#   shovel element into array
+
+# if element is not unique
+
+#   find the array it goes with
+
+#   shovel element into array
+
+end
+
+# when all elements are scored
+
+#   get length of each array
+
+#   sort arrays by length
+
+#   pick longest array
+
+#   print one of the elements
